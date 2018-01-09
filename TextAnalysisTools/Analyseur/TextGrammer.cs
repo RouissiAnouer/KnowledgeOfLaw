aee8470b0d54afcbea8d0eb8feea805e041c4d77
Rouissi Anouer <anouerrouissi@yahoo.fr>
2018-01-09 20:30:51 +0100

    Update TextGrammer.cs

--- TextAnalysisTools/Analyseur/TextGrammer.cs
+++ TextAnalysisTools/Analyseur/TextGrammer.cs
@@ -28,72 +28,7 @@ namespace TextAnalysisTools.Analyseur
                 
 
 
-                //new LexicalRule {Type=TokenType.Paragraph,RegExpression=new Regex("([aA-zZ]+'*[aA-zZ]* )+(\\s)*|([aA-zZ]+ )+[aA-zZ]*") },
-
-                
-
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                //new LexicalRule {Type = TokenType.WhiteSpace, RegExpression = new Regex("^\\s") }, // Whitespace
-                /*new LexicalRule { Type = TokenType.Operator, RegExpression = new Regex("^(and|or|not|is)\\b") }, // Word Operator
-                //new LexicalRule { Type = TokenType.Operator, RegExpression = new Regex("^[\\+\\-\\/%&|\\^~<>!]") }, // Single Char Operator
-                new LexicalRule { Type = TokenType.Operator, RegExpression = new Regex("^((==)|(!=)|(<=)|(>=)|(<>)|(<<)|(>>)|(//)|(\\*\\*))") }, // Double Char Operator
-                new LexicalRule { Type = TokenType.Delimiter, RegExpression = new Regex("^[\\(\\)\\[\\]\\{\\}@,:`=;\\.]") }, // Single Delimiter
-                new LexicalRule { Type = TokenType.Delimiter, RegExpression = new Regex("^((\\+=)|(\\-=)|(\\*=)|(%=)|(/=)|(&=)|(\\|=)|(\\^=))") }, // Double Char Operator
-                new LexicalRule { Type = TokenType.Delimiter, RegExpression = new Regex("^((//=)|(>>=)|(<<=)|(\\*\\*=))") }, // Triple Delimiter
-                new LexicalRule { Type = TokenType.Keyword, RegExpression = LexicalRule.WordRegex("as", "assert", "break", "class", "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "global", "if", "import", "lambda", "pass", "raise", "return", "try", "while", "with", "yield", "in", "print") }, // Keywords
-                new LexicalRule {
-                    Type = TokenType.Builtins,
-                    RegExpression = LexicalRule.WordRegex(
-                                      "ArithmeticError", "AssertionError", "AttributeError", "BaseException",
-                                      "BufferError", "BytesWarning", "DeprecationWarning", "EOFError", "Ellipsis",
-                                      "EnvironmentError", "Exception", "False", "FloatingPointError", "FutureWarning",
-                                      "GeneratorExit", "IOError", "ImportError", "ImportWarning", "IndentationError",
-                                      "IndexError", "KeyError", "KeyboardInterrupt", "LookupError", "MemoryError",
-                                      "NameError", "None", "NotImplemented", "NotImplementedError", "OSError",
-                                      "OverflowError", "PendingDeprecationWarning", "ReferenceError", "RuntimeError",
-                                      "RuntimeWarning", "StandardError", "StopIteration", "SyntaxError",
-                                      "SyntaxWarning", "SystemError", "SystemExit", "TabError", "True", "TypeError",
-                                      "UnboundLocalError", "UnicodeDecodeError", "UnicodeEncodeError", "UnicodeError",
-                                      "UnicodeTranslateError", "UnicodeWarning", "UserWarning", "ValueError", "Warning",
-                                      "WindowsError", "ZeroDivisionError", "_", "__debug__", "__doc__", "__import__",
-                                      "__name__", "__package__", "abs", "all", "any", "apply", "basestring", "bin",
-                                      "bool", "buffer", "bytearray", "bytes", "callable", "chr", "classmethod",
-                                      "cmp", "coerce", "complex", "copyright", "credits", "delattr",
-                                      "dict", "divmod", "enumerate", "filter", "float",
-                                      "format", "frozenset", "getattr", "hasattr",
-                                      "hash", "hex", "id", "int", "intern", "isinstance", "issubclass",
-                                      "iter", "len", "license", "list", "long", "map", "max", "memoryview",
-                                      "min", "next", "object", "oct", "ord", "pow", "print", "property",
-                                      "range", "raw_input", "reduce", "repr", "reversed",
-                                      "round", "set", "setattr", "slice", "sorted", "staticmethod", "str", "sum",
-                                      "super", "tuple", "type", "unichr", "unicode", "xrange", "zip") },
-                //new LexicalRule { Type = TokenType.Identifier, RegExpression = new Regex("^[_A-Za-z][_A-Za-z0-9]*") }, // Identifier
-                new LexicalRule { Type = TokenType.String, RegExpression = new Regex("^((@'(?:[^']|'')*'|'(?:\\.|[^\\']|)*('|\\b))|(@\"(?:[^\"]|\"\")*\"|\"(?:\\.|[^\\\"])*(\"|\\b)))", RegexOptions.IgnoreCase) }, // String Marker
-                
-                //new LexicalRule { Type = TokenType.Unknown, RegExpression = new Regex("^.") }, // Any*/
+              
             };
 
         }
