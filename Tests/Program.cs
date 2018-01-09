aaec462fda485c476d55417908fc8f4b0e3720c2
Rouissi Anouer <anouerrouissi@yahoo.fr>
2018-01-09 20:27:32 +0100

    Update Program.cs

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -37,44 +37,4 @@ namespace ConsoleTest
             Console.ReadLine();
         }
     }
-}/*
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TextAnalysisTools;
-
-namespace ConsoleTest
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var lines = new List<string>();
-
-            Console.WriteLine("Enter python script to tokenize below. Leave line empty to tokenize");
-
-            string line;
-            do
-            {
-                Console.Write("> ");
-                line = Console.ReadLine();
-
-                lines.Add(line);
-
-            } while (!string.IsNullOrEmpty(line));
-
-            var script = string.Join("\n", lines);
-
-            var tokenizer = new TextAnalysisTools.Tokenizer(new TextAnalysisTools.Analyseur.TextGrammer());
-
-            foreach (var token in tokenizer.Tokenize(script))
-            {
-                Console.WriteLine("{0}:{1} - {2} => {3}", token.StartIndex, token.Length, token.Type, script.Substring(token.StartIndex, token.Length));
-            }
-
-            Console.ReadLine();
-        }
-    }
-}*/
+}
